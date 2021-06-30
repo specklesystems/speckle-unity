054fe68fc3527f5e75d809eadabaa92fcdb81cec
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2021-06-30 17:40:38 -0500

    feat: non leaking meshes generated in editor mode
    Addressed issue with unity bugging out over meshes being created in editor mode by modifying the conversion process ConverterUnity.Geometry.cs. Also modified stream manager to manage the RecursiveConverter.cs component access

--- Assets/Speckle Connector/StreamManager.cs
+++ Assets/Speckle Connector/StreamManager.cs
@@ -5,30 +5,39 @@ using Speckle.Core.Credentials;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Speckle.Core.Models;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Events;
 
-namespace Speckle.ConnectorUnity
-{
-  [ExecuteAlways]
-  public class StreamManager : MonoBehaviour
-  {
+namespace Speckle.ConnectorUnity {
+    [ExecuteAlways]
+    public class StreamManager : MonoBehaviour {
 
-    public int SelectedAccountIndex = -1;
-    public int SelectedStreamIndex = -1;
-    public int SelectedBranchIndex = -1;
-    public int SelectedCommitIndex = -1;
-    public int OldSelectedAccountIndex = -1;
-    public int OldSelectedStreamIndex = -1;
-    
-    public Client Client;
-    public Account SelectedAccount;
-    public Stream SelectedStream;
+        public int SelectedAccountIndex = -1;
+        public int SelectedStreamIndex = -1;
+        public int SelectedBranchIndex = -1;
+        public int SelectedCommitIndex = -1;
+        public int OldSelectedAccountIndex = -1;
+        public int OldSelectedStreamIndex = -1;
 
-    public List<Account> Accounts;
-    public List<Stream> Streams;
-    public List<Branch> Branches;
+        public Client Client;
+        public Account SelectedAccount;
+        public Stream SelectedStream;
 
-  }
+        public List<Account> Accounts;
+        public List<Stream> Streams;
+        public List<Branch> Branches;
+
+        public GameObject ConvertRecursivelyToNative( Base @base, string id )
+            {
+            
+                var rc = GetComponent<RecursiveConverter>( );
+                if ( rc == null )
+                    rc = gameObject.AddComponent<RecursiveConverter>( );
+                
+                return rc.ConvertRecursivelyToNative( @base,
+                    Branches[ SelectedBranchIndex ].commits.items[ SelectedCommitIndex ].id );
+            }
+    }
 }
--- Assets/Speckle Connector/Utils.cs
+++ Assets/Speckle Connector/Utils.cs
@@ -1,23 +1,69 @@
 ﻿using System;
 using UnityEngine;
 
-namespace Speckle.ConnectorUnity
-{
-  public static class Utils
-  {
-    public static int ToIntColor(this Color c)
-    {
-      return
-        System.Drawing.Color
-          .FromArgb(Convert.ToInt32(c.r * 255), Convert.ToInt32(c.r * 255), Convert.ToInt32(c.r * 255))
-          .ToArgb();
-    }
+namespace Speckle.ConnectorUnity {
+    public static class Utils {
 
-    public static Color ToUnityColor(this int c)
-    {
-      var argb = System.Drawing.Color.FromArgb(c);
-      return new Color(argb.R / 255.0f, argb.G / 255.0f, argb.B / 255.0f);
+        public static void SafeDestroy( UnityEngine.Object obj )
+            {
+                if ( Application.isPlaying )
+                    UnityEngine.Object.Destroy( obj );
+
+                else
+                    UnityEngine.Object.DestroyImmediate( obj );
+
+            }
+
+        public static bool Valid( this string name ) => !string.IsNullOrEmpty( name );
+
+        public static Mesh SafeMeshGet( this MeshFilter mf ) => Application.isPlaying ? mf.mesh : mf.sharedMesh;
+
+        
+        
+        public static void SafeMeshSet( this GameObject go, Mesh m, bool addMeshFilterIfNotFound )
+            {
+                
+                var mf = go.GetComponent<MeshFilter>( );
+                if ( mf == null ) {
+                    if(!addMeshFilterIfNotFound) return;
+
+                    mf = go.AddComponent<MeshFilter>( );
+                }
+
+
+                if ( Application.isPlaying )
+                    mf.mesh = m;
+                else
+                    mf.sharedMesh = m;
+            }
+
+        
+        public static void SafeMeshSet( this GameObject go, Mesh m )
+            {
+                var mf = go.GetComponent<MeshFilter>( );
+                if ( mf == null ) return;
+
+
+                if ( Application.isPlaying )
+                    mf.mesh = m;
+                else
+                    mf.sharedMesh = m;
+            }
+
+
+        public static int ToIntColor( this Color c )
+            {
+                return
+                    System.Drawing.Color
+                        .FromArgb( Convert.ToInt32( c.r * 255 ), Convert.ToInt32( c.r * 255 ), Convert.ToInt32( c.r * 255 ) )
+                        .ToArgb( );
+            }
+
+        public static Color ToUnityColor( this int c )
+            {
+                var argb = System.Drawing.Color.FromArgb( c );
+                return new Color( argb.R / 255.0f, argb.G / 255.0f, argb.B / 255.0f );
+            }
+
     }
-    
-  }
 }
