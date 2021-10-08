b1cff8d00564866ae907decede5d07a4e6c497e5
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2021-10-07 20:44:48 -0500

    fix: moved speckle properties into observable collection

--- Assets/Speckle Connector/SpeckleProperties.cs
+++ Assets/Speckle Connector/SpeckleProperties.cs
@@ -1,14 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace Speckle.ConnectorUnity
 {
+
   /// <summary>
   /// This class gets attached to GOs and is used to store Speckle's metadata when sending / receiving
   /// </summary>
-  public class SpeckleProperties : MonoBehaviour
+  public class SpeckleProperties : MonoBehaviour, ISerializationCallbackReceiver
   {
-    public Dictionary<string, object> Data { get; set; }
+
+    [SerializeField, HideInInspector]
+    private SpeckleObservablePropertiesCollection observablePropertiesCollection;
+
+    [SerializeField, HideInInspector]
+    private string _serializedData;
+
+    public object this[string key]
+    {
+      get => observablePropertiesCollection[key];
+      set => observablePropertiesCollection[key] = value;
+    }
+
+    public Dictionary<string, object> Data
+    {
+      get { return observablePropertiesCollection.ToDictionary(); }
+      set { observablePropertiesCollection.Set(value); }
+    }
+
+    public void OnBeforeSerialize()
+    {
+      _serializedData = observablePropertiesCollection.serializedData;
+    }
+
+    public void OnAfterDeserialize()
+    {
+      observablePropertiesCollection.Set(_serializedData);
+      _serializedData = null;
+    }
+    
+    private void Awake()
+    {
+      observablePropertiesCollection = new SpeckleObservablePropertiesCollection();
+    }
+
   }
 }
---
+++ Assets/Speckle Connector/SpeckleProperty.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Speckle.ConnectorUnity
+{
+  [Serializable]
+  public class SpeckleProperty : INotifyPropertyChanged
+  {
+    private string key;
+    private object value;
+
+    public SpeckleProperty(string k, object v)
+    {
+      key = k;
+      value = v;
+    }
+
+    public string Key
+    {
+      get => key;
+      set
+      {
+        key = value;
+        OnPropertyChanged("Key");
+      }
+    }
+
+    public object Value
+    {
+      get => value;
+      set
+      {
+        this.value = value;
+        OnPropertyChanged("Value");
+      }
+    }
+
+    [field: NonSerialized]
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public void OnPropertyChanged(string name)
+    {
+      var handler = PropertyChanged;
+      handler?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+
+  }
+}
