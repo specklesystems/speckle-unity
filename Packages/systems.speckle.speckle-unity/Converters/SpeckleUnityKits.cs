417d69b5c650cc2a11fb9af360b6dd9f388c5b29
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2021-12-06 14:51:42 -0600

    Kit manager object for editor

---
+++ Packages/systems.speckle.speckle-unity/Converters/SpeckleUnityKits.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Objects.Converter.Unity
+{
+  [CreateAssetMenu(menuName = "Speckle/Create Speckle Kit Manager", fileName = "KitsAndConverters", order = 0)]
+  public class SpeckleUnityKits : ScriptableObject
+  {
+    public List<SpeckleConvertersSO> converters;
+
+    public static SpeckleUnityKits Instance { get; private set; }
+
+    public void OnEnable()
+    {
+      Instance = this;
+      
+      Debug.Log("enablecalled");
+    }
+
+  }
+}
