7edabae2ac63edc144e2fc36f9c63c3bf2a3f490
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2021-12-06 14:49:12 -0600

    added speckle converter so
    base converter wrapper that uses scriptable object

---
+++ Packages/systems.speckle.speckle-unity/Converters/SpeckleConvertersSO.cs
@@ -0,0 +1,18 @@
+﻿using Speckle.Core.Models;
+using UnityEngine;
+
+namespace Objects.Converter.Unity
+{
+  public interface IConvertMono
+  {
+    public GameObject ConvertToMono(Base @base);
+    public bool CanConvertToMono(Base @base);
+    
+  }
+  
+  public abstract class SpeckleConvertersSO : ScriptableObject, IConvertMono
+  {
+    public abstract GameObject ConvertToMono(Base @base);
+    public abstract bool CanConvertToMono(Base @base);
+  }
+}
